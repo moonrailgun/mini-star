daca4269015c6e57df85c98a67d68492e892dd45
moonrailgun <moonrailgun@gmail.com>
2020-05-19 12:06:26 +0800

    update import from other path

--- plugins/rollup.config.js
+++ plugins/rollup.config.js
@@ -3,10 +3,23 @@ import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import ptconfig from '../.ptconfig.json';
+import fs from 'fs';
 
 const scope = ptconfig.scope;
 const name = process.env.npm_package_name.replace(`@${scope}/`, '');
 const main = process.env.npm_package_main;
+
+const config = JSON.parse(fs.readFileSync('./package.json', 'utf8')) || {};
+const rootConfig =
+  JSON.parse(fs.readFileSync('../../package.json', 'utf8')) || {};
+const apps = fs.readdirSync('../');
+const deps = Object.keys(rootConfig.devDependencies || {})
+  .concat(Object.keys(rootConfig.dependencies || {}))
+  .concat(Object.keys(rootConfig.peerDependencies || {}))
+  .concat(Object.keys(config.devDependencies || {}))
+  .concat(Object.keys(config.dependencies || {}))
+  .concat(Object.keys(config.peerDependencies || {}))
+  .concat(apps.map((x) => `@plugin/${x}`));
 
 /**
  * reset amd id to uniq with `pluginName/fileName`
@@ -37,10 +50,21 @@ export default {
     },
     sourcemap: true,
   },
-  plugins: [typescript(), resolve({ browser: true }), commonjs(), replaceId()],
+  plugins: [
+    typescript({ noResolve: true }),
+    resolve({ browser: true }),
+    commonjs(),
+    replaceId(),
+  ],
   external: (id) => {
-    if (id.startsWith('@plugins/')) {
-      return true;
+    console.log('id', id);
+
+    return true; // TODO
+
+    for (const x of deps) {
+      if (id.startsWith(x)) {
+        return true;
+      }
     }
 
     return false;
