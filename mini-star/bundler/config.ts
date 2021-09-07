119f0ae2a0e488cacccc2cc0f84e968e6fff0c42
moonrailgun <moonrailgun@gmail.com>
2021-09-07 21:12:50 +0800

    feat: #20 add buildRollupPlugins which can rebuild all plugins order

--- mini-star/bundler/config.ts
+++ mini-star/bundler/config.ts
@@ -1,19 +1,32 @@
 import path from 'path';
 import { cosmiconfigSync } from 'cosmiconfig';
+import { Plugin as RollupPlugin } from 'rollup';
 const explorer = cosmiconfigSync('ministar');
 
-const defaultConfig = {
+interface MiniStarConfig {
+  scope: string;
+  pluginRoot: string;
+  outDir: string;
+  externalDeps: string[];
+  author?: string;
+  license?: string;
+  rollupPlugins: [];
+  buildRollupPlugins?: (defaultPlugins: RollupPlugin[]) => RollupPlugin[];
+}
+
+const defaultConfig: MiniStarConfig = {
   scope: 'plugins',
   pluginRoot: process.cwd(),
   outDir: path.resolve(process.cwd(), './dist'),
-  externalDeps: [] as string[],
+  externalDeps: [],
   author: undefined,
   license: undefined,
   rollupPlugins: [],
+  buildRollupPlugins: undefined,
 };
 
 const configResult = explorer.search();
-const config: typeof defaultConfig = {
+const config: MiniStarConfig = {
   ...defaultConfig,
   ...configResult?.config,
 };
