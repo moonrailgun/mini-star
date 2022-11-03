19a757ad280fb938a1aa7bf389d5a31bed6f1fcb
moonrailgun <moonrailgun@gmail.com>
2022-11-03 20:21:55 +0800

    chore: init playground with react

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,15 +1,34 @@
-import type { Component } from 'solid-js';
-import Editor from './components/Editor';
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
 
-const App: Component = () => {
+function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <>
-      <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
-      <div class="h-40">
-        <Editor url={`file:///mini-star/main`} />
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-    </>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
   );
-};
+}
 
 export default App;
